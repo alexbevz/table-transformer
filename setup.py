610c05498cab66dbce6202b27a57c96e5926bfaf
Александр Бевз <asbevz@istokmw.ru>
2024-06-25 16:04:22 +0300

    'Rofl'

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+def get_requirements() -> list[str]:
+    with open('detr/requirements.txt', 'r') as f:
+        return [line for line in f.readlines() if not line.startswith('git')]
+
+
+setup(
+    name='table-converter',
+    version='0.1',
+    description='Convert the tables at image to the structure tables',
+    packages=find_packages(),
+    author_email='as-bivz@yandex.ru',
+    zip_safe=False,
+    install_requires=get_requirements(),
+    python_requires='>=3.8'
+)
